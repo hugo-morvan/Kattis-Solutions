451c2bfe456c85c9940f574861c106542372bec8
Hugo Morvan <hugo.morvan@coyotes.usd.edu>
2023-11-20 16:24:47 +0100

    Fixed build.py

--- build.py
+++ build.py
@@ -52,12 +52,11 @@ for path, dirs, files in os.walk('source'):
 #print("Content lenght: ")
 #print(len(contents))
 
-HIDDEN = 0 # 19 + 37 <- ??
-lines = open('README.md', 'r').readlines()#[:3] #Change this if you want to keep some lines of code intact
+lines = open('README.md', 'r').readlines()[:0] #Change this if you want to keep some lines of code intact
 with open('README.md', 'w+') as f:
     for line in lines: f.write(line)
-    f.write(f'## Total problems solved: {len(contents) + HIDDEN}\n\n')
+    f.write(f'## Total problems solved: {len(contents)}\n\n')
     f.write(f'Note that the table below is auto-generated. There might be slight inaccuracies.\n\n')
-    f.write(f'auto-table script credits : https://github.com/RussellDash332/kattis.\n\n')
+    #f.write(f'auto-table script credits : https://github.com/RussellDash332/kattis.\n\n')
     f.write('|Problem Name|Problem ID|Languages|\n|:---|:---|:---|\n')
     for key, content in sorted(contents): f.write(content)
