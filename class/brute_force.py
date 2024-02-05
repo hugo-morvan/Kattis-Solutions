6e03ff9ac4cdc395866bc91b9a51a85d498f72b6
Hugo Morvan <Hugo.Morvan@coyotes.usd.edu>
2024-02-05 17:18:22 +0100

    lab 4 almost done

--- class/brute_force.py
+++ class/brute_force.py
@@ -1,3 +1,7 @@
+"""
+Short scrript to create a dictionary of followers for each word in the text
+"""
+
 from text_stats import *
 from tqdm import tqdm
 words_list = []
@@ -6,13 +10,11 @@ with open(file, "r", encoding="utf-8") as f:
     text = f.read()
     words_list = raw_text_to_clean_list(text)
 
-unique_words = dict_count_words(words_list)
-print(len(unique_words))
 brute_force_dict = {}
-for key,val in tqdm(unique_words):
-    if key not in brute_force_dict:
-        followers = dict_of_following_words(words_list, key)
-        brute_force_dict[key] = dict(followers)
+for word in tqdm(words_list):
+    if word not in brute_force_dict:
+        followers = dict_of_following_words(words_list, word)
+        brute_force_dict[word] = dict(followers)
 
-with open("followers.txt", 'w') as f:
-    f.write(brute_force_dict)
+with open("followers.txt", 'w', encoding='utf-8') as f:
+    f.write(str(brute_force_dict))
--- class/generate_text.py
+++ class/generate_text.py
@@ -4,7 +4,7 @@ from text_stats import *
 import sys
 import random
 import time
-from tqdm import tqdm
+#from tqdm import tqdm
 
 def main():
     if (len(sys.argv) < 4):
@@ -17,28 +17,56 @@ def main():
         print(f"Using file '{file}' to generate {max_token} words, starting from the word '{start_word}'.")
         start_time = time.time()
 
+        #--------Idea 1: use memoization to speedup the generation of the text.----------
+        # Time to generate 500 words: 15s, time to generate 2000 words: 45s
         #1) Get text and process it
-        words_list = []
-        with open(file, "r") as f:
-            text = f.read()
-            words_list = raw_text_to_clean_list(text)
+        #words_list = []
+        #with open(file, "r") as f:
+        #    text = f.read()
+        #    words_list = raw_text_to_clean_list(text)
+        #
+        #last_word = start_word
+        #seen_words = {}
+        #token = 0
+        #memo_count = 0
+        #while token < max_token:
+        #    last_word_followers = {}
+#
+        #    #Memoization :
+        #    if last_word not in seen_words.keys():
+        #        last_word_followers = dict_of_following_words(words_list, last_word)
+        #        seen_words[last_word] = last_word_followers
+        #        memo_count += 1
+        #    else:
+        #        last_word_followers = seen_words[last_word]
+#
+        #    last_word_followers = dict(last_word_followers)
+        #    num_followers = sum([val for val in last_word_followers.values()])
+        #    tup = list(last_word_followers.items())
+        #    pop, wei = zip(*tup)
+        #    next_word = random.choices(population = pop, weights = wei)[0]
+        #    print(next_word, end=' ')
+        #    last_word = next_word
+        #    token += 1
         
-        #Idea : use memoization to speedup the generation of the text.
+        #--------Idea 2: use a dictionary to store the followers of each word.(brute force)----------
+        #Time to generate the brute force dict: 10min
+        #Time to generate 500 words: 1.1s, time to generate 2000 words:1.3s
+        
+        with open("followers.txt", "r", encoding='utf-8') as f:
+            dict_w = f.read()
+            brute_force_dict = eval(dict_w)
+            #print(dict_w)
+        
         last_word = start_word
-        seen_words = {}
         token = 0
-        memo_count = 0
         while token < max_token:
             last_word_followers = {}
-
-            #Memoization :
-            if last_word not in seen_words.keys():
-                last_word_followers = dict_of_following_words(words_list, last_word)
-                seen_words[last_word] = last_word_followers
-                memo_count += 1
+            if last_word in brute_force_dict.keys():
+                last_word_followers = brute_force_dict[last_word]
             else:
-                last_word_followers = seen_words[last_word]
-
+                print(f"| last word {last_word} not in dictionary")
+                break
             last_word_followers = dict(last_word_followers)
             num_followers = sum([val for val in last_word_followers.values()])
             tup = list(last_word_followers.items())
@@ -47,9 +75,10 @@ def main():
             print(next_word, end=' ')
             last_word = next_word
             token += 1
+        
         print()
         print("Generation took --- %s seconds ---" % (time.time() - start_time))
-        print(f"Memoization dict used {memo_count} times")
+        #print(f"Memoization dict used {memo_count} times")
     return 0
 
 if __name__ == "__main__":
