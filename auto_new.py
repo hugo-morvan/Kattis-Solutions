0162479f57e0f74a62cf876d42236d1c3742eab4
Hugo Morvan <hugo.morvan@coyotes.usd.edu>
2024-11-14 16:24:59 +0100

    removed ?

--- auto_new.py
+++ auto_new.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+This script automatically chooses a new problem for you to solve.
+The suggestion is made using the suggest function from autokattis.
+It then creates a folder in your source directory, and a script inside that folder of your chosen progranning language.
+Finally, it prints the link to the problem description both in the file and in the terminal,
+as well as the command to submit your code from the terminal using the KattisCLI library.
+By default, the problem chosen is of trivial difficulty, but this can be changed with the DIFFICULTY variable.
+By default, the programming language for the file creates is python, but this can be changed with the PROG variable
+
+"""
+
+DIFFICULTY = 0 # 0-1 trivial, 2-3 easy, 4-5 moderate, 6-7 hard
+PROG = '.py' # change to desired programming language file extension
+INCLUDE_PB_LINK_IN_FILE = True
+INCLUDE_PB_LINK_IN_CONSOLE = False
+INCLUDE_SUBMIT_COMMAND = True
+SOURCE_LOCATION = "./source/" #change to desired source directory
 import os
 from dotenv import load_dotenv
 from autokattis import OpenKattis
@@ -13,28 +30,31 @@ password = os.getenv("PASSWORD")
 kt = OpenKattis(user, password)
                     
 def main():
-    """ Main program """
+
     sug = kt.suggest()
 
-    print(f"Selecting problem {sug[0]['name']} \n")
-    print("link: ",sug[0]["link"],"\n")
+    print(f"\nSelecting problem {sug[DIFFICULTY]['name']} \n")
+
+    if INCLUDE_PB_LINK_IN_CONSOLE:
+        print("link: ",sug[DIFFICULTY]["link"],"\n")
     
-    source_location = "./source/"                   #change to desired source directory
-    path = source_location + sug[0]['name']
-    file_name = sug[0]['pid']+".py"
+    path = SOURCE_LOCATION + sug[DIFFICULTY]['name']
+    file_name = sug[DIFFICULTY]['pid'] + PROG
     file_path = os.path.join(path, file_name) 
 
-    clean_path = source_location + "'" + sug[0]['name'] + "'" #to take care of the spaces in folders names
+    clean_path = SOURCE_LOCATION + "'" + sug[DIFFICULTY]['name'] + "'"
     clean_file_path = os.path.join(clean_path, file_name) 
-    submit_command = "kattis " + clean_file_path + " -f"
-    print(submit_command)
+
+    if INCLUDE_SUBMIT_COMMAND:
+        submit_command = "kattis " + clean_file_path + " -f\n"
+        print("Submit problem using:\n")
+        print(submit_command)
 
     if os.path.exists(path):
-        print("A directory already exists for this problem !")
-
+        print("A directory already exists for this problem !\n")
     else:
         os.makedirs(path)
-        print("The new directory is created!")
+        print("The new directory is created!\n")
         with open(file_path, 'w') as f: 
             f.write(f'#{sug[0]["link"]}')
 
